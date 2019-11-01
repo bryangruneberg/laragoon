eaa3e1a15060a397ef35d76b6fe5b0eda3827b9c
Bryan Gruneberg <hello@bryangruneberg.com>
2019-11-01 20:48:31 +0000

    Alter the DotEnv override to use the create static method

--- src/LaragoonService.php
+++ src/LaragoonService.php
@@ -93,7 +93,7 @@ class LaragoonService
         // Load environment variables on top of `.env` if it exists.
         $envFile = '.env.' . $lagoonEnvironment;
         if (file_exists(base_path() . DIRECTORY_SEPARATOR . $envFile)) {
-            $dotenv = new \Dotenv\Dotenv(base_path(), $envFile);
+            $dotenv = \Dotenv\Dotenv::create(base_path(), $envFile);
             $dotenv->overload();
         }
     }
